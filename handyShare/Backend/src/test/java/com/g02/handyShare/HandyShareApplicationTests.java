53896d7a9d2a381272a0200abfd52aaf114852f1
Vaibhavkumar <vaibhavpatel162002@gmail.com>
2024-09-30 15:13:01 -0300

    Project Setup

---
+++ handyShare/Backend/src/test/java/com/g02/handyShare/HandyShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g02.handyShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
