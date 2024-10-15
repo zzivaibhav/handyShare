a4c0713eba409d9b5e489ebc1abe49f18a21e929
zzivaibhav <vaibhavpatel162002@gmail.com>
2024-10-14 23:22:56 -0300

    Feat():Added the homepage frontend for the application

--- handyShare/Frontend/src/App.js
+++ handyShare/Frontend/src/App.js
@@ -3,15 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup.tsx'; 
 import Login from './pages/Login.tsx';
 import ForgotPassword from './pages/ForgotPassword.tsx';
+import HomeScreen from './pages/HomeScreen.js';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Signup />} /> {/* Add a route for the root path */}
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-    </Routes>
+<HomeScreen/>
+    // <Routes>
+    //   <Route path="/" element={<Signup />} /> {/* Add a route for the root path */}
+    //   <Route path="/signup" element={<Signup />} />
+    //   <Route path="/login" element={<Login />} />
+    //   <Route path="/forgot-password" element={<ForgotPassword />} />
+    //   <Route path = "/home-page" element={HomeScreen}/>
+    // </Routes>
   );
 }
 
