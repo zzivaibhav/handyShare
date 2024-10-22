e624e597dbfe1c2a3090fd969e4be3eff6a3e70c
Shivam13602 <157157200+Shivam13602@users.noreply.github.com>
2024-10-22 13:30:37 -0300

    MOdified UI module to add Date and rent button

--- handyShare/Frontend/src/components/ProductPage/ProductDetails.js
+++ handyShare/Frontend/src/components/ProductPage/ProductDetails.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export function ProductDetails({ name, price, transactionTime }) {
+export function ProductDetails({ name, price, transactionTime, description }) {
   return (
     <div>
       <h2>{name}</h2>
       <p>Price: ${price}</p>
       <p>Transaction Time: {transactionTime}</p>
+      <p style={{ textAlign: 'center', fontSize: '14px', fontWeight: 'normal', marginBottom: '5px' }}>
+        {description || 'No description available.'}
+      </p>
     </div>
   );
 }
