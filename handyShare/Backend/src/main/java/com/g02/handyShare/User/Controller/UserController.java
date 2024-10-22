e6128468443f5ddd5de6a7f6e7bfee19eee464a3
Vaibhavkumar Patel <76529210+zzivaibhav@users.noreply.github.com>
2024-10-22 11:40:41 -0300

    path fix

--- handyShare/Backend/src/main/java/com/g02/handyShare/User/Controller/UserController.java
+++ handyShare/Backend/src/main/java/com/g02/handyShare/User/Controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 //API starting with /admin are accessible by admin only.
 
 @RestController
- @RequestMapping("/api/v1/all")
+ @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
 public class UserController {
 
@@ -25,7 +25,7 @@ public class UserController {
     @Autowired
     UserRepository repo;
 
-    @PostMapping("/register")
+    @PostMapping("/all/register")
     public ResponseEntity<String> registerUser( @RequestBody User user) {
 
         if (user.getEmail() == null || user.getEmail().isEmpty()) {
