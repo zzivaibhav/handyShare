3832fe00bf4a672d7f666577c1fa13af07cec1db
Vaibhavkumar Patel <76529210+zzivaibhav@users.noreply.github.com>
2024-11-07 01:12:03 -0400

    chore: make routes protected from the front-end side.

--- handyShare/Frontend/src/App.js
+++ handyShare/Frontend/src/App.js
@@ -15,27 +15,31 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import Feedback from './pages/Feedback.js';
 import RentSummaryPage from './pages/RentSummaryPage.js';
 import OAuth2RedirectHandler from "../src/components/ProfileUpdatePage/OAuth2RedirectHandler.js"
+import ProtectedRoute from './components/ProtectedRoute.js';
+import { AuthProvider } from './context/AuthContext.js';
 
 function App() {
   return (
+    <AuthProvider>
     <Routes>
       <Route path="/" element={<Signup />} /> {/* Add a route for the root path */}
       <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} /> 
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path = "/homepage" element={<HomeScreen/>}/>
-      <Route path="/profile" element={<Profile/>} />
-      <Route path = "/profile-update" element={<ProfileUpdate/>}/>
+      <Route path = "/homepage" element={<ProtectedRoute><HomeScreen/></ProtectedRoute>}/>
+      <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>} />
+      <Route path = "/profile-update" element={<ProtectedRoute><ProfileUpdate/></ProtectedRoute>}/>
       <Route path="/admin" element={<AdminDashboard/>} />
-      <Route path="/product/:id" element={<ProductPage />} />
-      <Route path="/lend" element={<LendPage />} />
-      <Route path="/products" element={<ProductsListPage />} />
-      <Route path="/payment" element={<Payment/>} />
-      <Route path="/feedback" element={<Feedback/>} />
-      <Route path="/rent-summary" element={<RentSummaryPage/>} />
-      <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler/>}/>
+      <Route path="/product/:id" element={<ProtectedRoute><ProductPage /></ProtectedRoute>} />
+      <Route path="/lend" element={<ProtectedRoute><LendPage /></ProtectedRoute>} />
+      <Route path="/products" element={<ProtectedRoute><ProductsListPage /></ProtectedRoute>} />
+      <Route path="/payment" element={<ProtectedRoute><Payment/></ProtectedRoute>} />
+      <Route path="/feedback" element={<ProtectedRoute><Feedback/></ProtectedRoute>} />
+      <Route path="/rent-summary" element={<ProtectedRoute><RentSummaryPage/></ProtectedRoute>} />
+      <Route path="/oauth2/redirect" element={<ProtectedRoute><OAuth2RedirectHandler/></ProtectedRoute>}/>
 
     </Routes>
+    </AuthProvider>
   );
 }
 
