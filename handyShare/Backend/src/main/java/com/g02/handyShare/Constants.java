78bbb11afb6f0bbf0b55b5947b6733ee60256552
Yash Harjani <yashharjani2001@gmail.com>
2024-11-08 15:01:08 -0400

    Adding unit test cases

--- handyShare/Backend/src/main/java/com/g02/handyShare/Constants.java
+++ handyShare/Backend/src/main/java/com/g02/handyShare/Constants.java
@@ -13,4 +13,16 @@ public class Constants {
     public final String SECRET = "AFSDVS";
     public final String FRONT_END_HOST="http://localhost:3000";
     public final String SERVER_URL = "http://localhost:8080";
+
+    public String getSecret() {
+        return SECRET;
+    }
+
+    public String getFrontEndHost() {
+        return FRONT_END_HOST;
+    }
+
+    public String getServerUrl() {
+        return SERVER_URL;
+    }
 }
