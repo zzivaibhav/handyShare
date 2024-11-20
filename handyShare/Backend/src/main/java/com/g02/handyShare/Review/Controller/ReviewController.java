94e27ad8ecf74d7c3a422e3abfd64dcb528ac971
AkshitaMendon <akshitamendon025@gmail.com>
2024-11-20 13:21:04 -0400

    Modification in the code to upload image

--- handyShare/Backend/src/main/java/com/g02/handyShare/Review/Controller/ReviewController.java
+++ handyShare/Backend/src/main/java/com/g02/handyShare/Review/Controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.g02.handyShare.Review.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.g02.handyShare.Config.Firebase.FirebaseService;
 import com.g02.handyShare.Review.Dto.ReviewRequest; 
 import com.g02.handyShare.Review.Dto.ReviewResponse;
 import com.g02.handyShare.Review.Dto.ReviewWithUserDTO;
@@ -25,7 +30,13 @@ import com.g02.handyShare.Review.Service.ReviewService;
 public class ReviewController {
 
     @Autowired
-    private ReviewService reviewService;  
+    private ReviewService reviewService; 
+    private FirebaseService firebaseService;
+    
+    @Autowired
+    public void Controller(FirebaseService firebaseService){
+        this.firebaseService=firebaseService;
+    }
 
     // Endpoint to get reviews for a product
     @GetMapping("/review-product/{productId}")
@@ -41,7 +52,11 @@ public class ReviewController {
 
     // Endpoint to create new review
     @PostMapping("/review-create")
-    public ResponseEntity<ReviewResponse> createReview(@RequestBody ReviewRequest reviewRequest) {
+    public ResponseEntity<ReviewResponse> createReview(@RequestBody ReviewRequest reviewRequest, @RequestPart(value="reviewimage", required = false) MultipartFile file) throws IOException {
+        if (file!=null){
+            String url=firebaseService.uploadFile(file, "/reviews");
+            reviewRequest.setImage(url);
+        }
         // Using ReviewService to create the review
         Review review = reviewService.createReview(
                 reviewRequest.getUserId(),
