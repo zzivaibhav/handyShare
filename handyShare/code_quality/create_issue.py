be2eabaa0ecf84bf8af9d9651bc075bf151a96a9
Vaibhavkumar Patel <76529210+zzivaibhav@users.noreply.github.com>
2024-11-22 21:33:36 -0400

    Update and rename issue.py to create_issue.py

---
+++ handyShare/code_quality/create_issue.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import requests
+from csv2md import table
+import csv
+
+commit = sys.argv[1]
+pat = os.environ.get("PAT")
+path_to_smells = "smells/"
+smell_files = [f for f in os.listdir(path_to_smells) if os.path.isfile(
+    os.path.join(path_to_smells, f)) and str(f).strip().endswith(".csv")]
+
+headers = {
+    "Authorization": f"Bearer {pat}",
+    "Accept": "application/vnd.github+json",
+}
+for sf in smell_files:
+
+    with open(os.path.join(path_to_smells, sf)) as csv_file:
+        list_smells = list(csv.reader(csv_file))
+
+    raw_md = table.Table(list_smells).markdown()
+
+    title = str(sf).replace(".csv", "")+" for commit - "+str(commit)
+    body = {
+        "title": title,
+        "body": raw_md
+    }
+    github_output = requests.post(
+        f"https://api.github.com/repos/MootezSaaD/lab-8-code-smells/issues", headers=headers, json=body)
+    if not github_output.status_code == 201:
+        raise Exception
