4d674e68cea5557dc2b78a172ab394df68e0d961
Yash Harjani <yashharjani2001@gmail.com>
2024-11-28 19:31:34 -0400

    test(error solving)

--- handyShare/Backend/src/test/java/com/g02/handyShare/TestConstants.java
+++ handyShare/Backend/src/test/java/com/g02/handyShare/TestConstants.java
@@ -1,5 +1,5 @@
 package com.g02.handyShare;
 
 public class TestConstants {
-    public static final String BASE_URL = "http://172.17.0.99:%s/api/v1";
+    public static final String BASE_URL = "http://localhost:%s/api/v1";
 }
